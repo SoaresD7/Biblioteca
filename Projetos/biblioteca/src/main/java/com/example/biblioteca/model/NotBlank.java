dae38f9e5412202cf89a19e72ebb07f45aa8f494
Davi Soares <Davi.soares2008@icloud.com>
2025-06-15 11:04:03 -0300

    Delete Projetos directory

--- Projetos/biblioteca/src/main/java/com/example/biblioteca/model/NotBlank.java
+++
@@ -1,5 +0,0 @@
-package com.example.biblioteca.model;
-
-public @interface NotBlank {
-
-}
--- Projetos/biblioteca/src/main/java/com/example/biblioteca/repository/LivroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.biblioteca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.biblioteca.model.LivroModel;
-
-public interface LivroRepository extends JpaRepository<LivroModel, Long> {
-}
